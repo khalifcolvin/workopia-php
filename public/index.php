16c13db6fc3807131f92a0debfb21b3b6cf4d7e8
khalifcolvin <khalifcolvin925@gmail.com>
2024-01-18 14:07:50 +0700

    Help debug please :)

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
 require '../helpers.php';
 
-require basePath('views/home.view.php');
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+inspectAndDie($uri);
